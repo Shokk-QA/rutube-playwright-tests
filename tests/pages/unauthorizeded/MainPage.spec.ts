44a7f74e35f25f20d8d066291f2e8c72b3592928
Shokk-QA <danil.darks.ask@gmail.com>
2025-10-22 15:01:50 +0300

    addMainPageFixtures

--- tests/pages/unauthorizeded/MainPage.spec.ts
+++ tests/pages/unauthorizeded/MainPage.spec.ts
@@ -1,24 +1,12 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../MainPage';
 
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await page.getByRole('button', { name: 'Закрыть' }).click();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.hederHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await page.getByRole('button', { name: 'Закрыть' }).click();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await page.getByRole('button', { name: 'Закрыть' }).click();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
