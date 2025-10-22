e9ed73c84c4e8f45263e84352a506d9996eaf439
Shokk-QA <danil.darks.ask@gmail.com>
2025-10-22 13:09:38 +0300

    MainPagesTest

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -1,8 +1,29 @@
-import { Page } from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class MainPage extends BasePage {
+  private readonly headerLocator: Locator;
+  private readonly categoriesTabLocator: Locator;
+  private readonly menuLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.headerLocator = this.page.getByRole('banner');
+    this.categoriesTabLocator = this.page.locator('section').filter({
+      hasText: /^ГлавнаяФильмыСериалыТелешоуСпортБлогерыНовостиМузыкаПодкастыДетямТВ онлайн$/,
+    });
+    this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+  }
   async open() {
     await this.page.goto('https://rutube.ru/');
+  }
+  async hederHasCorrectAriaSnapshot() {
+    await expect(this.headerLocator).toMatchAriaSnapshot();
+  }
+  async categoriesTabHasCorrectAriaSnapshot() {
+    await expect(this.categoriesTabLocator).toMatchAriaSnapshot();
+  }
+  async menuHasCorrectAriaSnapshot() {
+    await expect(this.menuLocator).toMatchAriaSnapshot();
   }
 }
--- tests/pages/unauthorizeded/MainPage.spec.ts
+++ tests/pages/unauthorizeded/MainPage.spec.ts
@@ -1,7 +1,24 @@
 import test from '@playwright/test';
 import { MainPage } from '../MainPage';
 
-test('Открытие главной страницы', async ({ page }) => {
+test('Проверка доступности элементов хедера', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.open();
+  await page.getByRole('button', { name: 'Закрыть' }).click();
+  await mainPage.closeCookiesAlert();
+  await mainPage.hederHasCorrectAriaSnapshot();
+});
+test('Проверка доступности элементов табов категорий', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await page.getByRole('button', { name: 'Закрыть' }).click();
+  await mainPage.closeCookiesAlert();
+  await mainPage.categoriesTabHasCorrectAriaSnapshot();
+});
+test('Проверка доступности элементов бокового меню', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.open();
+  await page.getByRole('button', { name: 'Закрыть' }).click();
+  await mainPage.closeCookiesAlert();
+  await mainPage.menuHasCorrectAriaSnapshot();
 });
